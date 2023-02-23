7f219a94ea81f0044e82298b2100528b77956bca
kimdozzi <neo5188@gmail.com>
2023-02-23 10:29:18 +0900

    각p120829 각도기 문제풀이

--- src/main/java/com/ll/level0/p120807/Main.java
+++ src/main/java/com/ll/level0/p120807/Main.java
@@ -2,9 +2,8 @@ package com.ll.level0.p120807;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(new Solution().solution(2,3));
-    }
-}
+//        System.out.println(new Solution().solution(2,3));
+    }}
 
 class Solution {
     public int solution(int num1,int num2) {
