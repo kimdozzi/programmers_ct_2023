91787719003eff8821548eb9cc1961e6c415d17f
kimdozzi <neo5188@gmail.com>
2023-03-06 23:32:14 +0900

    p16236 힌트보고 ê²í해결 다ì

--- src/main/java/com/ll/LeetCode/easy/p35/Main.java
+++ src/main/java/com/ll/LeetCode/easy/p35/Main.java
@@ -1,7 +1,7 @@
 package com.ll.LeetCode.easy.p35;
 
 public class Main {
-    class Solution {
+    static class Solution {
         public int searchInsert(int[] nums, int target) {
             int left = 0, right = nums.length - 1;
 
@@ -19,5 +19,10 @@ public class Main {
             }
             return left;
         }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,3,5,6};
+        new Solution().searchInsert(arr, 2);
     }
 }
