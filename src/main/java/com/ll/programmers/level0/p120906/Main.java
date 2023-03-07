d39d8e8e9789d16cbebdef940e85d248ee8bfe04
kimdozzi <neo5188@gmail.com>
2023-03-07 20:32:50 +0900

    TDD 방식으로 p12ã programmers/06 문제 풀이

--- src/main/java/com/ll/programmers/level0/p120906/Main.java
+++ src/main/java/com/ll/programmers/level0/p120906/Main.java
@@ -2,10 +2,16 @@ package com.ll.programmers.level0.p120906;
 
 public class Main {
     public static void main(String[] args) {
-
     }
 }
 
 class Solution {
-
+    public int solution(int n) {
+        int answer = 0;
+        String s = n + "";
+        for(int i = 0; i < s.length(); i++) {
+            answer += Character.getNumericValue(s.charAt(i));
+        }
+        return answer;
+    }
 }
--- src/test/java/com/ll/programmers/level0/p120906/Tests.java
+++ src/test/java/com/ll/programmers/level0/p120906/Tests.java
@@ -1,13 +1,20 @@
 package com.ll.programmers.level0.p120906;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class Tests {
-    @test
+    @Test
     @DisplayName("1234 => 10")
     void t1() {
         assertThat(new Solution().solution(1234)).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("930211 => 16")
+    void t2() {
+        assertThat(new Solution().solution(930211)).isEqualTo(16);
     }
 }
